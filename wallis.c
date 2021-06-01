e17a898c1a81e77c3134e14b2ec052f0e6f37bf5
devika-s00 <84584568+devika-s00@users.noreply.github.com>
2021-06-01 10:22:29 +0530

    wallis soln

--- wallis.c
+++ wallis.c
@@ -18,7 +18,7 @@ return value*2;
 
 int main(void) {
   float pi;
-  for (int i=1; i<6; i++) {
+  for (int i=0; i<5; i++) {
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) > 0.15)) {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
